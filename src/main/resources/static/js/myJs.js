f75690fa2b6f7de0e50abf22245e64d445bae5d7
jb7959 <jb7959@naver.com>
2016-06-03 21:25:00 +0900

    '강사후보자명상태변환추가'

--- src/main/resources/static/js/myJs.js
+++ src/main/resources/static/js/myJs.js
@@ -3,26 +3,40 @@
  */
 
 //선택한 값 넘겨주기
-function getSelectValue()
-{
+function getSelectValue() {
     var target = document.getElementById("selectedInstructor");
-    var selectedInstructorId= target.options[target.selectedIndex].id;
+    var selectedInstructorId = target.options[target.selectedIndex].id;
     alert(selectedInstructorId);
-    document.getElementById("phone").value = document.getElementById("pn"+ selectedInstructorId).innerHTML;
+    document.getElementById("phone").value = document.getElementById("pn" + selectedInstructorId).innerHTML;
 }
 
 //AJAX를 사용한 강사연락처 가져오기 기능
 function loadDoc() {
     var target = document.getElementById("selectedInstructor");
     /*alert("Start"+target.selectedIndex)*/
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            alert("work")
-            document.getElementById("phone").value = xhttp.responseText;
-        }
-    };
-    xhttp.open("POST", "../findInstructorsPhoneNumber", true);
-   /* xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 헤더설정시*/
-    xhttp.send(target.selectedIndex);
+    if (target.selectedIndex != 0) {
+
+        var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    document.getElementById("phone").value = xhttp.responseText;
+                }
+            };
+        xhttp.open("POST", "../findInstructorsPhoneNumber", true);
+        /* xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 헤더설정시*/
+        xhttp.send(target.selectedIndex);
+    }
 }
+//document.getElementById("selectedInstructor").addEventListener("click", function(){} 과 같음
+$('#selectedInstructor').on('click', function () {
+    var target = document.getElementById("selectedInstructor");
+    if (target.selectedIndex == 0) {
+        document.getElementById("status2").style.visibility='hidden'
+        document.getElementById("status1").style.visibility='visible'
+    } else {
+        document.getElementById("status1").style.visibility='hidden'
+        document.getElementById("status2").style.visibility='visible'
+    }
+
+
+});
