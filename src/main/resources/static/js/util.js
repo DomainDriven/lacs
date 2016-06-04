91177ef8c670d57544534f46fd4de050f8e46ba9
devarchi33 <devarchi33@gmail.com>
2016-06-04 21:18:39 +0900

    1. 뒤로가기 함수 추가 및 적용.

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -20,3 +20,12 @@ function toggleBtn(complete, restart) {
         completeBtn.css("display", "block");
     });
 }
+
+/**
+ * backBtn class 를 붙인 tag 를 클릭하면 뒤로가기.
+ *
+ * css class tag 만 추가하면 동작시키키 위해서 function 으로 감싸지 않음.
+ */
+$(".backBtn").click(function () {
+    history.back();
+});
