b77c790b1739add85cf807f8895b7e291289bcc6
jb7959 <jb7959@naver.com>
2016-07-20 21:33:59 +0900

    홍보자료 수정 기능 추가

--- src/main/resources/static/js/promoting.js
+++ src/main/resources/static/js/promoting.js
@@ -2,6 +2,8 @@
  * Created by jerry on 2016-07-14.
  */
 $(window).load(function () {
+    var lastIndex;
+
     $(function () {
         $('#promotingInfo').DataTable({
             "paging": true,
@@ -12,14 +14,34 @@ $(window).load(function () {
             "autoWidth": false
         });
     });
+    //수정 클릭시 데이터 호출
+    $(".modifyingPromotion").on("click",function(){
+        var indexNumber = $(this).attr("value");
+        lastIndex = indexNumber;
+        var locationName = $("#locationName"+indexNumber).text();
+        var locationURL = $("#locationURL"+indexNumber).text();
+        var worker = $("#worker"+indexNumber).text();
+        $("#locationNameInputForm").val(locationName);
+        $("#locationURLInputForm").val(locationURL);
+        $("#workerInputForm").val(worker);
+    });
 
+    $("#modifyPromotionsSubmit").on("click",function(){
+        var title = $("#titleInfo").text();
+        alert(lastIndex+"삭제후생성");
+        promotingDelete(title,lastIndex);
+    });
+    
+    //홍보자료 삭제 버튼
     $(".deletingPromotion").on("click",function(){
         var indexNumber = $(this).attr("value");
         var title = $("#titleInfo").text();
+        alert(indexNumber+"삭제");
         console.log(indexNumber+"$$"+title);
         promotingDelete(title,indexNumber);
+        location.reload(); //페이지 다시실행
     });
-
+    //홍보자료 삭제를 위한 함수
     function promotingDelete(title,indexNumber) {
         $.ajax({
             type: "POST",
@@ -27,7 +49,7 @@ $(window).load(function () {
             data: {"title": title,"indexNumber": indexNumber},
             dataType: "html",
             success: function () {
-                location.reload(); //페이지 다시실행
+                alert("홍보자료삭제성공!");
             }
         });
     }
