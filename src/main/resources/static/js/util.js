17e3535c026eb60c125a0f265623309de4f66979
devarchi33 <devarchi33@gmail.com>
2016-07-27 23:57:17 +0900

    1. dateRangePicker 가 적용되어 버그가 있었던 부분을 datePicker 를 적용하여 수정. 2. util.js  에 있던 함수들을 Lacs 의  prototype 함수 로 등록 후 index.js 리팩토링.

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -2,73 +2,7 @@
  * Created by donghoon on 2016. 6. 4..
  */
 
-/**
- * ui toggle btn.
- * @param completeBtn
- * @param restartBtn
- */
-function toggleBtn(completeBtn, restartBtn, nextBtn, completeVal, restartVal, disableVal) {
-    var progressBar = $(".progress-bar");
-    var title = $("#currentSeminarTitle").val();
-    var order = $("#taskOrder").val();
-
-    completeBtn.click(function () {
-        $(this).css("display", "none");
-        restartBtn.css("display", "block");
-        progressBar.attr("aria-valuenow", completeVal)
-            .css("width", completeVal + "%").text(completeVal + "%");
-        ajaxWrapper("/seminar/taskProgress", {
-            title: title,
-            order: order,
-            progress: completeVal
-        }, function () {
-            nextBtn.attr("disabled", disableVal);
-        });
-
-    });
-    restartBtn.click(function () {
-        $(this).css("display", "none");
-        completeBtn.css("display", "block");
-        progressBar.attr("aria-valuenow", restartVal)
-            .css("width", restartVal + "%").text(restartVal + "%");
-        ajaxWrapper("/seminar/taskProgress", {
-            title: title,
-            order: order,
-            progress: restartVal
-        }, function () {
-            nextBtn.attr("disabled", !disableVal);
-        });
-    });
-}
-
-/**
- * backBtn class 를 붙인 tag 를 클릭하면 뒤로가기.
- *
- * css class tag 만 추가하면 동작시키키 위해서 function 으로 감싸지 않음.
- */
-$(".backBtn").click(function () {
-    history.back();
-});
-
-/**
- * ajax call 에서 공통부분을 뽑아낸 함수.
- * @param url
- * @param successFunc
- * @param errorFunc
- */
-function ajaxWrapper(url, data, successFunc, errorFunc) {
-    $.ajax({
-        type: "post",
-        async: true,
-        dataType: "json",
-        url: url,
-        data: data,
-        success: successFunc,
-        error: errorFunc
-    })
-}
-
 // 날짜입력 Datepicker
-$(function() {
-    $( "#date,#dateModal" ).datepicker();
+$(function () {
+    $("#date,#dateModal").datepicker();
 });
